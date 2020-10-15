c6a1090348b7a3d025aebe479acfc1c5dd86a810
chaudo_a <a.chaudoreille@gmail.com>
2020-10-15 14:24:33 +0200

    minor fixes: variable renaming and comments

--- lawn-mower/v2/app.py
+++ lawn-mower/v2/app.py
@@ -6,33 +6,37 @@ def error(*args, **kwargs):
     """ prints an error message to stderr """
     print(*args, file=sys.stderr, **kwargs)
 
-def mower_algorithm(grid, initial_position, instructions):
+def mower(lawn, initial_position, instructions):
     orientations = ('N','E','S','W')
     forward = {
+        # north : y += 1
         'N': lambda pos,grid: {
             'x': pos['x'],
             'y': pos['y'] + 1 if pos['y'] < grid['y'] else pos['y'],
             'o': pos['o']
         },
+        # south : y -= 1
         'E': lambda pos,grid: {
             'x': pos['x'] + 1 if pos['x'] < grid['x'] else pos['x'],
             'y': pos['y'],
             'o': pos['o']
         },
+        # east  : x += 1 
         'S': lambda pos,grid: {
             'x': pos['x'],
             'y': pos['y'] - 1 if pos['y'] > 0 else pos['y'],
             'o': pos['o']
         },
+        # west  : x -= 1 
         'W': lambda pos,grid: {
             'x': pos['x'] - 1 if pos['x'] > 0 else pos['x'],
             'y': pos['y'],
             'o': pos['o']
         },
     }
-    grid_size = {
-        'x': int(grid[0]),
-        'y': int(grid[1])
+    grid = {
+        'x': int(lawn[0]),
+        'y': int(lawn[1])
     }
     position = {
         'x': int(initial_position[0]),
@@ -40,13 +44,17 @@ def mower_algorithm(grid, initial_position, instructions):
         'o': orientations.index(initial_position[2]),
     }
 
+    # position['o'] stores the index of the orientation letter in the orientations tuple
+    # using modulo on indexes allows to cycle on the tuple instead of overflowing
     for step in instructions:
         if (step == 'L'):
+            # go left in the orientations list when rotating the mower counter-clockwise
             position['o'] = (position['o'] - 1) % 4
         elif step == 'R':
+            # go right when rotating the mower clockwise
             position['o'] = (position['o'] + 1) % 4
         else:
-            position = forward[orientations[position['o']]](position, grid_size)
+            position = forward[orientations[position['o']]](position, grid)
 
     return '{0} {1} {2}'.format(position['x'], position['y'], orientations[position['o']])
 
@@ -54,6 +62,7 @@ def mower_generator(filepath):
     grid = None
     initial_position = None
     instructions = None
+
     try:
         with open(filepath) as file:
             for line_number, line in enumerate(file):
@@ -75,7 +84,7 @@ def mower_generator(filepath):
                     if re.match(r'^[LRF]+$', line):
                         instructions = list(line.strip())
 
-                        yield mower_algorithm(grid, initial_position, instructions)
+                        yield mower(grid, initial_position, instructions)
                     else:
                         # @todo: proper error message
                         error(f'parsing error line {line_number}')
