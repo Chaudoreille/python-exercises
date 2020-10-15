fca439f217a97fa0d8857cb3f57fabd1f2ef2c16
chaudo_a <a.chaudoreille@gmail.com>
2020-10-15 12:21:26 +0200

    removed old file lawn_mower.py and created directory and app file for v2

---
+++ lawn-mower/v2/app.py
@@ -0,0 +1,21 @@
+import sys
+import re
+
+# @todo: create a better usage function 
+def usage():
+    print('you need to provide an input file')
+
+def token_generator(filename):
+
+def main():
+    if len(sys.argv) != 2:
+        usage()
+    else:
+        filepath = sys.argv[1]
+
+
+    
+
+
+if __name__ == 'main':
+    main()
