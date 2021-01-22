ff59c840262556ef0cd46c13007ce1fa57843f8e
Chaudo <a.chaudoreille@gmail.com>
2021-01-22 10:32:54 +0100

    :sparkles: cuboid exercice for list comprehensions

--- hackerrank/list_comprehensions/cuboid.py
+++ hackerrank/list_comprehensions/cuboid.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     z = int(raw_input())
     n = int(raw_input())
     
-    
+    [[i,j,k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
