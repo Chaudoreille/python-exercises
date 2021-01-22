aafcc6cba4ad229afade73d8696410a1c88f32c2
Chaudo <a.chaudoreille@gmail.com>
2021-01-22 10:19:40 +0100

    :tada: starting hackerrank problems

---
+++ hackerrank/list_comprehensions/cuboid.py
@@ -0,0 +1,10 @@
+# problem :
+# https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+if __name__ == '__main__':
+    x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+    
+    
