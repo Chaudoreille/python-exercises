9090fa82065ce78a6369d83cff4f8c9fe6c9a8f2
Chaudo <a.chaudoreille@gmail.com>
2021-02-06 19:52:14 +0100

    :construction: changed class name from Node to BinaryTree. Implemented min(),max() and height() methods and finished remove()

--- data_structures/binary_tree.py
+++ data_structures/binary_tree.py
@@ -1,4 +1,4 @@
-class Node:
+class BinaryTree:
     def __init__(self, data):
         self.left = None
         self.right = None
@@ -7,16 +7,28 @@ class Node:
     def binary_search(self, data):
         pass
 
+    def min(self):
+        if self.left:
+            return self.left.min()
+        else:
+            return self.data
+
+    def max(self):
+        if self.right:
+            return self.right.min()
+        else:
+            return self.data
+
     def insert(self, data):
         if self.data:
             if data < self.data:
                 if self.left is None:
-                    self.left = Node(data)
+                    self.left = BinaryTree(data)
                 else:
                     self.left.insert(data)
             elif data > self.data:
                 if self.right is None:
-                    self.right = Node(data)
+                    self.right = BinaryTree(data)
                 else:
                     self.right.insert(data)
         else:
@@ -29,4 +41,24 @@ class Node:
             elif data > self.data:
                 self.right = self.right.remove(data)
             else:
-                
+                if not self.left and not self.right:
+                    return None
+                elif self.right:
+                    self.data = self.right.min()
+                    self.right = self.right.remove(self.data)
+                else:
+                    self.data = self.left.max()
+                    self.left = self.left.remove(self.data)
+        return self
+                    
+    def height(self):
+        if not self.left and not self.right:
+            return 1
+        elif not self.right:
+            return self.left.height + 1
+        elif not self.left:
+            return self.right.height + 1
+        else:
+            return max(self.left.height(), self.right.height()) + 1
+
+ 
